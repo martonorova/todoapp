a7c88b1e18d14dc7a3ed43b07098ecbe91c3e4b2
Marton Orova <marton.orova@gmail.com>
2018-10-01 23:35:09 +0200

    Basic functionality done

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Note(models.Model):
+    note_title = models.CharField(max_length=50)
+    note_text = models.CharField(max_length=200)
+    date_created = models.DateTimeField()
+    is_done = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.note_title
